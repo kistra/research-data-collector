b8c49f07256ea447554d76008e22cf7dfa018bef
Matej Susilovic <Matej.Susilovic@infobip.com>
2016-09-01 16:44:30 +0200

    - added cross origin annotation

--- src/main/java/researchDataCollector/testController/TestController.java
+++ src/main/java/researchDataCollector/testController/TestController.java
@@ -1,6 +1,7 @@
 package researchDataCollector.testController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import researchDataCollector.research.ResearchEntity;
@@ -10,6 +11,7 @@ import researchDataCollector.variable.VariableRepository;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class TestController {
 
